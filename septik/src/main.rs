19eb79d1450319add684126f65e31cc499631cf7
Eric Olson <eric.rob.olson@gmail.com>
2020-05-23 10:36:08 -0700

    Added in simple voxel generation.

--- septik/src/main.rs
+++ septik/src/main.rs
@@ -7,9 +7,6 @@ extern crate three;
 use std::env;
 use three::Object;
 fn main() {
-    let mut args = env::args();
-    let obj_path = concat!(env!("CARGO_MANIFEST_DIR"), "/test_data/untitled.obj");
-    let path = args.nth(1).unwrap_or(obj_path.into());
     let mut win = three::Window::new("Three-rs obj loading example");
     win.scene.background = three::Background::Color(0xFFFFFF);
 
@@ -24,13 +21,37 @@ fn main() {
 
     win.scene.add(&dir_light);
 
-    let root = win.factory.group();
-    win.scene.add(&root);
-    let (mut group_map, _meshes) = win.factory.load_obj(&path);
-    for g in group_map.values_mut() {
-        root.add(g);
+    // Load a monkey for debugging
+    {
+        let mut args = env::args();
+        let obj_path = concat!(env!("CARGO_MANIFEST_DIR"), "/test_data/untitled.obj");
+        let path = args.nth(1).unwrap_or(obj_path.into());
+        let root = win.factory.group();
+        win.scene.add(&root);
+        let (mut group_map, _meshes) = win.factory.load_obj(&path);
+        for g in group_map.values_mut() {
+            root.add(g);
+        }
     }
 
+    // Add some simple voxels to play with
+    {
+        for x in 0..8 {
+            for y in 0..8 {
+                for z in 0..8 {
+                    let mbox = {
+                        let geometry = three::Geometry::cuboid(1.0, 1.0, 1.0);
+                        let material = three::material::Wireframe { color: 0x00FF00 };
+                        win.factory.mesh(geometry, material)
+                    };
+                    mbox.set_position([x as f32, y as f32, z as f32]);
+                    win.scene.add(&mbox);
+                }
+            }
+        }
+    }
+
+    // Render
     while win.update() && !win.input.hit(three::KEY_ESCAPE) {
         controls.update(&win.input);
         win.render(&cam);
